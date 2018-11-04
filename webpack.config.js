f26ba5fafcd1f8682869ad5eb880e1d34ce4cb3e
Chris McDonald <mcdonaldchristopherkevin@gmail.com>
2018-11-04 13:59:50 -0800

    Change entry file to html

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
         }),
 
         new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'index.ejs'),
+            template: path.join(__dirname, 'index.html'),
             title: appHtmlTitle
         })
     ],
